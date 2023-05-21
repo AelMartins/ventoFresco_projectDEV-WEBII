d91965fb1637b7814e424778bb55f28207317e29
AelMartins <oneapcwork@gmail.com>
2023-05-21 20:37:46 -0300

    started working environment

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Logo Vento Fresco" src="./assets/logo.svg">
+</template>
+
+<script>
+// import login from './components/loginTemplate.vue'
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/loginTemplate.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login">
+
+  </div>
+</template>
+
+<script>
+// export default {
+//   name: '',
+//   props: {
+  
+//   }
+// }
+// </script>
